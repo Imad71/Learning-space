b4c9e967e72d7cc1f844773d032065f5c1963df6
Imad71 <81964275+Imad71@users.noreply.github.com>
2021-07-22 22:29:00 +0100

    Create average_of_three_numbers.py
    Original commit

---
+++ average_of_three_numbers.py
@@ -0,0 +1,11 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+num3 = input('Enter third number: ')
+avr = (int(num1) + int(num2) + int(num3)) / 3
+print("the average is ==>" , avr)
+
+# Add two numbers13
+sum = float(num1) + float(num2) + float(num3)
+
+# Display the sum
+print('The sum of {0} and {1} and {2} is {3}'.format(num1, num2, num3, sum))
